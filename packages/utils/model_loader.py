f7ca8636e31b5c352059773cf409b7fe682a34d3
Simon Nguyen <80842534+cobeo2004@users.noreply.github.com>
2025-05-20 23:39:50 +1000

    fix: remove unused imports and update deps

--- packages/utils/model_loader.py
+++ packages/utils/model_loader.py
@@ -1,9 +1,3 @@
-from pathlib import Path
-from tkinter import Image
-from typing import Generic
-import numpy as np
-import torch
-from ultralytics import YOLO
 from ..interfaces.model_loader_interface import IModelLoader, ModelLoaderImageType
 
 
